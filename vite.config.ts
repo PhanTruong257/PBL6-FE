1b8ce2d3a3e94259f69a375d1c3cdbeb9ef4870a
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-10-03 23:09:35 +0700

    fix(config): fix vite config, vite port in .env

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,31 @@ import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({ autoCodeSplitting: true }),  // Scan src/routes, generate routeTree.gen.ts
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // Load biến môi trường từ file .env.[mode]
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      tanstackRouter({ autoCodeSplitting: true }),  // Scan src/routes, generate routeTree.gen.ts
+      viteReact(),
+      tailwindcss(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
     },
-  },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: Number(env.VITE_PORT) || 3000, // port động từ .env
+    },
+    mode, // gán lại mode để Vite biết bạn đang chạy ở môi trường nào
+  }
 })
